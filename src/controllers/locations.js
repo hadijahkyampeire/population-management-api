78c6b4069dce13ca8eaeaa399bcfe10233a11932
hadijahkyampeire <hadijah315@gmail.com>
2019-03-01 00:20:00 +0300

    Finalize location endpoints

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 
 const getSingleLocation = async req => {
   const { id } = req.params;
-  return await Location.getLocationById({ _id: id });
+  return await Location.getSingleForUser({ _id: id });
 };
 
 const locationRules = () => {
@@ -14,17 +14,21 @@ const locationRules = () => {
     females: 'required|min:1',
     males: 'required|min:1'
   };
+
   return commonRules;
 };
 
 module.exports = {
   // create a location
   async store(req, res, next) {
-    const { location_name, females, males, parent_locationId } = await validate(
-      req,
-      locationRules(rule('unique', Location))
-    );
-    const locationData = { location_name, females, males, parent_locationId };
+    var locationData = {
+      location_name: req.body.location_name,
+      females: req.body.females,
+      males: req.body.males,
+      parent_locationId: req.body.parent_locationId
+    };
+    await validate(req, locationRules(rule('unique', Location)));
+
     location = await Location.create(locationData);
     return res.status(201).send({ location, message: 'Location added successfully' });
   },
@@ -34,28 +38,55 @@ module.exports = {
     var males = _.sumBy(locations, 'males');
     var females = _.sumBy(locations, 'females');
     const total_residents = females + males;
-    console.log(females, males, total_residents);
     return res.json({ locations, females, males, total_residents });
   },
 
   //update a location by id
   async update(req, res, next) {
-    const data = await validate(req, locationRules());
+    const data = req.body;
     const location = await getSingleLocation(req);
 
     if (!location) {
       res.status(404).json({ error: "Location doesn't exist" });
     }
-    const updated = await location.update(data);
-    return res.json({ message: `${updated.nModified} attributes were updated` });
+    await location.update(data);
+    return res.json({ message: `Location was updated successfully`, updated: location });
   },
 
-  //get one location by id
+  //get one location by id, if it has nested locations it also brings them
   async show(req, res) {
-    const location = await getSingleLocation(req);
-    return location
-      ? res.json({ location })
-      : res.status(404).json({ error: 'location not found' });
-  }
+    const parentLocation = await getSingleLocation(req);
+    const location = await Location.getAllForLocation(req.params.id);
+    //TODO:proper nexting could be done here
+    if (location && location.length > 1) {
+      var males = _.sumBy(location, 'males');
+      var females = _.sumBy(location, 'females');
+      var total_residents = females + males;
+      return res.json({
+        Name: parentLocation.location_name,
+        location,
+        females,
+        males,
+        total_residents
+      });
+    }
+    if (location.length === 1) {
+      var female = location[0].females;
+      var male = location[0].males;
+      var Residents = female + male;
+
+      return res.json({ location, female, male, Residents });
+    }
+    return res.status(404).json({ error: 'location not found' });
+  },
   //delete a location
+  async destroy(req, res) {
+    const location = await getSingleLocation(req);
+    if (!location) {
+      return res.status(400).json({ error: "location doesn't exist" });
+    }
+    await Location.delete(location);
+
+    return res.json({ message: 'location was deleted successfully' });
+  },
 };
--- src/models/location.js
+++ src/models/location.js
@@ -4,15 +4,27 @@ var locationSchema = mongoose.Schema({
   location_name: { type: String, required: true },
   females: { type: Number, required: false },
   males: { type: Number, required: true },
-  parent_locationId: { type: String },
+  parent_locationId: { type: String, required: false },
   created: { type: Date, default: Date.now }
 });
 //get a single location
-locationSchema.static('getLocationById', function(id, callback) {
-  Location.findById(id, callback);
+// locationSchema.static('getLocationById', function(id, callback) {
+//   Location.findById(id, callback);
+// });
+
+locationSchema.static('getSingleForUser', function(filters = {}) {
+  return this.findOne({
+    ...filters
+  });
 });
 
-locationSchema.post('remove', async function(next) {
+locationSchema.static('getAllForLocation', function(id) {
+  return this.find({
+    $or: [{ _id: id }, { parent_locationId: id }],
+  });
+});
+
+locationSchema.post('delete', async function(next) {
   //listen for when a location is deleted and remove it if its a parent of another location
   await Location.deleteMany({ id: _id });
   await Location.updateMany({ parent_locationId: null });
