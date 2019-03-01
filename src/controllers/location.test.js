ccddc1343578815ac13f08797223e5fdae1acd86
hadijahkyampeire <hadijah315@gmail.com>
2019-03-01 16:38:27 +0300

    fix tests

--- src/controllers/location.test.js
+++ src/controllers/location.test.js
@@ -13,32 +13,32 @@ const user = {
 };
 const mockToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImVtYWlsIjoiaGFkaWphaEBnbWFpbC5jb20iLCJ1c2VyX2lkIjoiNWM3OTFkOTc3YzVhNzAwMDE3N2IwYTQyIn0sImlhdCI6MTU1MTQ0MjI1MCwiZXhwIjoxNTUxNTI4NjUwfQ.luSQeskvCbuICe9scaRsXW3iKOD-TEMWIefowh9KncI';
-const loginUser = () => {
-  return createUser().then(() => {
-    return agent
-      .post('/api/v1/login')
-      .send(user)
-      .set('Content-Type', 'application/json')
-      .then((response) => {
-        const payload = { email: 'hard@gmail.com', user_id: '5c77dcdbf7e86d33c977a877' };
-        token = jwt.sign({ payload }, process.env.SECRET, { expiresIn: 86400 });
-        console.log({ token }, response.body);
-        return response.body
-      })
-  });
-};
+// const loginUser = () => {
+//   return createUser().then(() => {
+//     return agent
+//       .post('/api/v1/login')
+//       .send(user)
+//       .set('Content-Type', 'application/json')
+//       .then((response) => {
+//         const payload = { email: 'hard@gmail.com', user_id: '5c77dcdbf7e86d33c977a877' };
+//         token = jwt.sign({ payload }, process.env.SECRET, { expiresIn: 86400 });
+//         console.log({ token }, response.body);
+//         return response.body
+//       })
+//   });
+// };
 
-const createUser = () => {
-  return agent
-    .post('/api/v1/signup')
-    .send(user)
-    .set('Content-Type', 'application/json');
-};
+// const createUser = () => {
+//   return agent
+//     .post('/api/v1/signup')
+//     .send(user)
+//     .set('Content-Type', 'application/json');
+// };
 
-beforeAll(async function() {
-  // set up here
-  token = await loginUser();
-});
+// beforeAll(async function() {
+//   // set up here
+//   token = await loginUser();
+// });
 
 describe('Locations', () => {
   test('post /location returns 201 for authorized user', done => {
